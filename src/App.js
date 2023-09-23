1a9a177e7d71bb741a99ae3980fc5f1d3ec1d487
sanmofe <santiagomorafelix@gmail.com>
2023-09-22 20:01:16 -0500

    Primer commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const [formValues, setFormValues] = useState({email:"", password:"", favClass:"1"})
+
+  const [validationStates, setValidationStates] = useState({emailState: false, passwordState: false});
+
+  const handleEmailChange = ((e) => {
+    setFormValues({...formValues, email: e.target.value})
+  });
+ 
+  const handlePasswordChange = ((e) => {
+    const regexPwd = /^(?=.*\d)(?=.*[a-z])(?=.*[a-zA-Z]).{8,}$/
+    if(regexPwd.test(e.target.value)){
+      setValidationStates({ ...validationStates, passwordState: true });
+    }else{
+      setValidationStates({ ...validationStates, passwordState: false });
+    }
+    setFormValues({...formValues, password: e.target.value})
+  });
+ 
+  const handleSelectChange = ((e) => {
+    setFormValues({...formValues, favClass: e.target.value})
+  });
+
+  const clickSubmit = (() => {
+    const regexCorreo = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+    if(regexCorreo.test(formValues.email)){
+      setValidationStates({ ...validationStates, emailState: true });
+      return;
+    }
+    alert("Invalid email!")
+  })
+
+ return (
+   <div>
+     <h1>Ejemplo de formularios!</h1>
+    
+     <Form>
+     <Form.Group className="mb-6" controlId="formBasicEmail">
+       <Form.Label>Email address</Form.Label>
+       <Form.Control type="email" placeholder="Enter email" onChange={handleEmailChange} value={formValues.email}/>
+       { !validationStates.emailState && <Form.Text className="text-muted">Your email should follow an established format:</Form.Text>}
+     </Form.Group>
+
+     <Form.Group className="mb-3" controlId="formBasicPassword">
+       <Form.Label>Password</Form.Label>
+       <Form.Control type="password" placeholder="Password" onChange={handlePasswordChange} value={formValues.password} />
+       { !validationStates.passwordState && <Form.Text className="text-muted">Your password should be have numbers and letters and should be at least 9 char long</Form.Text>}
+     </Form.Group>
+     <Form.Group className="mb-3" controlId="formBasicCheckbox">
+       <Form.Label>Favorite Class</Form.Label>
+       <Form.Select onChange={handleSelectChange}>
+         <option value="1">ISIS3710</option>
+         <option value="2">Programación con tecnologias web</option>
+       </Form.Select>
+     </Form.Group>
+     <Button variant="primary" onClick={clickSubmit}>
+       Submit
+     </Button>
+   </Form>
+   </div>
+ );
 }
 
 export default App;
